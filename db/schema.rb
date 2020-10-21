ecaba49e7f569888dce06dd0257a1850c43097b6
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-10-21 15:48:16 -0300

    fix gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_21_165114) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "likes", force: :cascade do |t|
     t.integer "tweet_id"
     t.integer "user_id"
