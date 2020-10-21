1eed4952c779334b6e412308876d36499b94feb6
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-10-21 18:35:27 -0300

    tweets_spec done

--- spec/views/tweets_spec.rb
+++ spec/views/tweets_spec.rb
@@ -21,15 +21,39 @@ describe 'the signin process', type: :feature do
     @relationship = Relationship.create!({ follower_id: @test_user1_follow.id,
                                        followed_id: @test_user1.id })
 
-    @post = Tweet.create!({ author_id: @test_user1_follow.id,
-                           content: 'Test post from Chuck' })
+    @tweet = Tweet.create!({ author_id: @test_user1_follow.id,
+                           content: 'Test post from Chuck', id: 1 })
+    visit 'users/sign_in'
+    fill_in 'Login', with: @test_user1.email
+    fill_in 'user_password', with: @test_user1.password
+    click_button('Log in')
   end
 
   it 'Display message when creating a tweet' do
     visit '/'
-
     find('.input', match: :first).set('Testing first tweet!')
-    page.find("#create-tweet").click
+    click_button("Create Tweet")
     expect(page).to have_content 'Tweet was successfully created.'
   end
+
+  it 'Display message when deleting a tweet' do
+    visit '/'
+    find('.input', match: :first).set('Testing first tweet!')
+    click_button("Create Tweet")
+    find('.delete').click
+    expect(page).not_to have_css(".like", text: "Testing first tweet!")
+   
+  end
+  it 'Display message when editing a tweet' do
+  
+    visit '/'
+    find('.input', match: :first).set('Testing first tweet!')
+    click_button("Create Tweet")
+    find('.edit').click
+
+    find('.input', match: :first).set('Editing first tweet!')
+    click_button("Update Tweet")
+    expect(page).to have_content 'Tweet was successfully updated.'
+  end
+
 end
