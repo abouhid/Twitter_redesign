8b5898ba47d353b5bfbf77e43fcf8aa549048906
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-10-21 12:59:04 -0300

    like button added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def unfollow!(other_user)
     relationships.find_by_followed_id(other_user.id).destroy
   end
+
+  def like_or_dislike_btn(tweet)
+    like = Like.find_by(tweet: tweet, user: current_user)
+    if like
+      link_to('👎', tweet_like_path(id: like.id, tweet_id: tweet.id), method: :delete,  class: "likes")
+    else
+      link_to('👍', tweet_likes_path(tweet_id: tweet.id), method: :tweet,  class: "likes")
+    end
+  end
 end
